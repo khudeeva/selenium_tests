cc126a3a4cc2fddc3a3f4f9b382daf1d1be03ec1
khudeeva <kskhudeeva@gmail.com>
2025-05-12 19:03:37 +0500

    Тренировка Selenium

---
+++ tests/test_practice_automation.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+def test_practice_automation():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://practicetestautomation.com/practice-test-login/")
+
+    driver.find_element(By.CSS_SELECTOR, "input[id='username']").send_keys("student")
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys("Password123")
+
+    driver.find_element(By.ID, "submit").click()
+   
+   
+    assert "logged-in-successfully" in driver.current_url
+    driver.quit()
