6890a281a9e98367c26ead4da3b82bd770820ce8
khudeeva <kskhudeeva@gmail.com>
2025-05-21 17:16:38 +0500

    Add test with switch tab

---
+++ tests/herokuapp/tabs_windows/test_switch_windows.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+def test_switch_windows():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://the-internet.herokuapp.com/windows")
+
+    original_tab = driver.current_window_handle
+
+    driver.find_element(By.LINK_TEXT, "Click Here").click()
+    WebDriverWait(driver, 10).until(lambda d: len(d.window_handles)>1)
+    for handle in driver.window_handles:
+        if handle != original_tab:
+            driver.switch_to.window(handle)
+            break
+    heading = driver.find_element(By.TAG_NAME, "h3")
+    assert "New Window" in heading.text.strip()
+
+    driver.switch_to.window(original_tab)
+    assert "/windows" in driver.current_url
+    driver.quit()
